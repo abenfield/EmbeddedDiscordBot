442d9cde9aab4de5c14afc2c0039c52e6a359f7a
Austin Benfield <austin@devblender.com>
2023-12-16 19:11:20 -0600

    fixes python or

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ async def on_message(message):
     elif message.content.startswith("https://x.com"):
         link = message.content
         new_link = link.replace("x", "fixupx")
-    elif message.content.startswith("https://reddit.com") || message.content.startswith("https://www.reddit.com"):
+    elif message.content.startswith("https://reddit.com") or message.content.startswith("https://www.reddit.com"):
         link = message.content
         new_link = link.replace("reddit", "rxddit")
     elif message.content.startswith("https://old.reddit.com"):
